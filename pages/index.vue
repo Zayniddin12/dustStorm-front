dc3253a7b881c944ace790db9037bbad03a0c34c
Zayniddin12 <masudxaltaca@gmail.com>
2025-02-16 15:21:42 +0300

    fix: err

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,10 @@
     <Contact /> -->
   </div>
 </template>
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import Event from '@/components/Sections/Main/Event.vue'
 import SectionsReport from '@/components/Sections/Report/Report.vue'
 import Contact from '@/components/Sections/Main/Contact.vue'
 import SeaAral from '@/components/Sections/Main/SeaAral.vue'
 import AnnualChart from '@/components/Sections/Main/AnnualChart.vue'
-</script>
+</script> -->
