f4101f5707f71429cdda2ac06330d59bf1f8bb84
Zayniddin12 <masudxaltaca@gmail.com>
2025-02-19 17:19:09 +0300

    fix: err

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   ssr: true,
 
   devtools: { enabled: false },
+
   app: {
-    // pageTransition: { name: 'fade', mode: 'out-in' },
     head: {
       htmlAttrs: {
         lang: 'en',
@@ -21,11 +21,6 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'format-detection', content: 'telephone=no' },
-        // {
-        //   hid: 'og:image',
-        //   property: 'og:image',
-        //   content: 'https://unit-study-front.uicgroup.tech/description.png',
-        // },
         {
           hid: 'og:title',
           property: 'og:title',
@@ -37,11 +32,6 @@ export default defineNuxtConfig({
           content:
             'Unity Study provides quality education consulting and guarantees study opportunities abroad, offering expert guidance for a successful international educational experience.',
         },
-        // {
-        //   hid: 'twitter:image',
-        //   property: 'twitter:image',
-        //   content: 'https://unit-study-front.uicgroup.tech/description.png',
-        // },
         {
           hid: 'twitter:image:alt',
           property: 'twitter:image:alt',
@@ -58,10 +48,6 @@ export default defineNuxtConfig({
           content:
             'Unity Study provides quality education consulting and guarantees study opportunities abroad, offering expert guidance for a successful international educational experience.',
         },
-        // {
-        //   property: 'og:image',
-        //   content: 'https://unit-study-front.uicgroup.tech/description.png',
-        // },
         {
           name: 'description',
           content:
@@ -76,64 +62,8 @@ export default defineNuxtConfig({
     'nuxt-simple-robots',
     'nuxt-simple-sitemap',
     'nuxt-marquee',
-    [
-      '@pinia/nuxt',
-      {
-        autoImports: [
-          // automatically imports `defineStore`
-          'defineStore', // import { defineStore } from 'pinia'
-          // automatically imports `defineStore` as `definePiniaStore`
-          ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-        ],
-      },
-    ],
-    [
-      {
-        id: 98656039,
-        webvisor: true,
-      },
-    ],
+    '@pinia/nuxt', // Pinia to'g'ri qo‘shilgan
   ],
-
-  // site: {
-  //   url: 'https://unit-study.uicgroup.tech/',
-  // },
-  // routeRules: {
-  //   '/': {
-  //     sitemap: {
-  //       changefreq: 'daily',
-  //       priority: 1,
-  //       lastmod: new Date().toString('yyyy-mm-ddThh:mm:ss:zzz'),
-  //     },
-  //   },
-  //   '/ru': {
-  //     sitemap: {
-  //       changefreq: 'daily',
-  //       priority: 1,
-  //       lastmod: new Date().toString('yyyy-mm-ddThh:mm:ss:zzz'),
-  //     },
-  //   },
-  //   '/en': {
-  //     sitemap: {
-  //       changefreq: 'daily',
-  //       priority: 1,
-  //       lastmod: new Date().toString('yyyy-mm-ddThh:mm:ss:zzz'),
-  //     },
-  //   },
-  // },
-  // sitemap: {
-  //   exclude: ['/profile/**', '/profile', '/edit/**', '/edit'],
-  //   xslColumns: [
-  //     { label: 'URL', width: '50%' },
-  //     { label: 'Last Modified', select: 'sitemap:lastmod', width: '25%' },
-  //     { label: 'Priority', select: 'sitemap:priority', width: '12.5%' },
-  //     {
-  //       label: 'Change Frequency',
-  //       select: 'sitemap:changefreq',
-  //       width: '12.5%',
-  //     },
-  //   ],
-  // },
 
   css: ['@/assets/tailwind.css'],
 
@@ -143,7 +73,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      baseURL: process.env.VITE_API_BASE_URL || 'localhost',
+      baseURL: process.env.VITE_API_BASE_URL || 'http://localhost',
     },
   },
 
@@ -160,5 +90,6 @@ export default defineNuxtConfig({
   yandexMaps: {
     apikey: '9191d860-42c5-4e70-98aa-8c92721bbdc3',
   },
+
   compatibilityDate: '2024-08-27',
 })
